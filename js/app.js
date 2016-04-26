1e754a8af8f60c6d9ee128a92ebf74d54b9e2e3e
tetsaiga86 <tetsaiga86@gmail.com>
2016-04-25 19:42:24 -0700

    fixed counters and put them in table to prove results

--- js/app.js
+++ js/app.js
@@ -3,12 +3,16 @@ var appendImg = document.getElementById('append-img');
 var appendImg2 = document.getElementById('append-img2');
 var appendImg3 = document.getElementById('append-img3');
 
+var pictureUniqueId = 0;
+
 function Image(name, fileType){
   this.picName = name;
   this.tag = fileType;
   this.path = 'img/' + this.picName + this.tag;
   this.counterClick = 0;
   this.counterAppear = 0;
+  this.id = pictureUniqueId;
+  pictureUniqueId = pictureUniqueId + 1;
 }
 
 pic.push(new Image('bag', '.jpg'));
@@ -35,7 +39,7 @@ pic.push(new Image('wine-glass', '.jpg'));
 var index = random(pic);   //gives us the index of a random Image object in the pic array
 
 var li = document.createElement('li');
-li.innerHTML = '<img src=' + pic[index].path + ' />';
+li.innerHTML = renderImage(index);
 appendImg.appendChild(li);
 
 var index2 = random(pic);
@@ -45,7 +49,7 @@ while (index2 === index) {
 }
 
 var li2 = document.createElement('li');
-li2.innerHTML = '<img src=' + pic[index2].path + ' />';
+li2.innerHTML = renderImage(index2);
 appendImg2.appendChild(li2);
 
 var index3 = random(pic);
@@ -55,7 +59,7 @@ while (index3 === index2 || index3 === index){
 }
 
 var li3 = document.createElement('li');
-li3.innerHTML = '<img src=' + pic[index3].path + ' />';
+li3.innerHTML = renderImage(index3);
 appendImg3.appendChild(li3);
 
 function random(allPics){
@@ -71,20 +75,69 @@ choosePic2.addEventListener('click', newImage);
 var choosePic3 = document.getElementById('append-img3');
 choosePic3.addEventListener('click', newImage);
 
-function newImage(){
+function newImage(event){
+  for (var i = 0; i < pic.length; i++) {
+    if (pic[i].id == event.target.id) {
+      pic[i].counterClick++;
+    }
+  }
   var index = random(pic);
-  li.innerHTML = '<img src=' + pic[index].path + ' />';
+  li.innerHTML = renderImage(index);
 
   var index2 = random(pic);
   while (index2 === index) {
     index2 = random(pic);
   }
-  li2.innerHTML = '<img src=' + pic[index2].path + ' />';
+  li2.innerHTML = renderImage(index2);
 
   var index3 = random(pic);
   while (index3 === index2 || index3 === index){
     index3 = random(pic);
   }
-  li3.innerHTML = '<img src=' + pic[index3].path + ' />';
-
+  li3.innerHTML = renderImage(index3);
 }
+
+function renderImage(index){
+  pic[index].counterAppear++;
+  return '<img src=' + pic[index].path + ' id="' + pic[index].id + '" />';
+}
+
+var resultsButton = document.getElementById('results-button');
+var resultsTable = document.getElementById('results-table');
+resultsButton.addEventListener('click', showResults);
+
+function showResults() {
+  resultsTable.innerHTML = '';
+  for (var i = 0; i < pic.length; i++) {
+    showResultRow(i);
+  }
+}
+
+function showResultRow(rowIndex){
+  var tr = document.createElement('tr');
+  var picName = document.createElement('td');
+  var clicks = document.createElement('td');
+  var shown = document.createElement('td');
+  picName.textContent = pic[rowIndex].picName;
+  clicks.textContent = pic[rowIndex].counterClick;
+  shown.textContent = pic[rowIndex].counterAppear;
+  tr.appendChild(picName);
+  tr.appendChild(clicks);
+  tr.appendChild(shown);
+  resultsTable.appendChild(tr);
+}
+
+//   for (var i = 0; i < store.hourlyCookies.length; i++) {
+//     var td = document.createElement('td');
+//     td.textContent = store.hourlyCookies[i];
+//     tr.appendChild(td);
+//   };
+//   var total = document.createElement('td');
+//   total.textContent = store.dailyTotal;
+//   tr.appendChild(total);
+//   if(replace){
+//     appendRows.replaceChild(tr,replace);
+//   }else{
+//     appendRows.appendChild(tr);
+//   }
+// }
